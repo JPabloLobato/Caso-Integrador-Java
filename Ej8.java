ddb12bf3d2351a7e4434e0523da64c6242051f23
JPabloLobato <146821982+JPabloLobato@users.noreply.github.com>
2024-02-07 19:00:55 +0100

    Ej 8 finalizado

--- Ej8.java
+++ Ej8.java
@@ -5,7 +5,7 @@ public class Ej8 {
         ArrayList<Integer> numeros = new ArrayList<>();
         Scanner scanner = new Scanner (System.in);
         int num;
-        System.out.prrintln ("Ingrese un número, al final introduce un 0: ");
+        System.out.println ("Ingrese un número, al final introduce un 0: ");
         do {
             num = scanner.nextInt();
             if (num != 0) {
@@ -14,6 +14,36 @@ public class Ej8 {
         } while (num != 0);
         return numeros;
         }
-        public static double calcular Media(ArrayList<Integer> numeros)
+        public static double calcularMedia(ArrayList<Integer> numeros) {
+            double suma = 0;
+            for (int i = 0; i < numeros.size(); i++) {
+                suma += numeros.get(i);
+            }
+            return suma / numeros.size();
+        }
+        public static int calcularMinimo(ArrayList<Integer> numeros) {
+            int min= numeros.get(0);
+            for (int i = 1; i < numeros.size(); i++) {
+                if (numeros.get(i) < min) {
+                    min = numeros.get(i);
+                }
+            }
+            return min;
+        }
+        public static int calcularMaximo(ArrayList<Integer> numeros) {
+            int max = numeros.get(0);
+            for (int i = 1; i < numeros.size(); i++) {
+                if (numeros.get(i) > max) {
+                    max = numeros.get(i);
+                }
+            }
+            return max;
+        }
+        public static void main(String[] args) {
+            ArrayList<Integer> numeros = recibirNumeros();
+            System.out.println("La media es: " + calcularMedia(numeros));
+            System.out.println("El mínimo es: " + calcularMinimo(numeros));
+            System.out.println("El máximo es: " + calcularMaximo(numeros));
+
     }
 }
